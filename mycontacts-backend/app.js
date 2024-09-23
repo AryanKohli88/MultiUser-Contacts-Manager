9326054e49cd17ba32cf94e360a5ea1c50d27816
Aryan Kohli <aryankohli0805@gmail.com>
2024-09-23 20:14:23 +0530

    to deploy

--- mycontacts-backend/app.js
+++ mycontacts-backend/app.js
@@ -4,15 +4,24 @@ const errorhandler = require("./middleware/errorhandler");
 const connectDb = require("./config/dbconnection");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 connectDb();
+
+app.use(cors({
+    origin: '*', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Enable cookies and other credentials
+  }));  
+
 
 app.use(express.json());
 app.use(errorhandler);
 app.use("/api/contacts", require("./routes/contacts_routes"));
 app.use("/api/users", require("./routes/user_routes"));
-app.listen(port, () => {
+app.get('/', (req,res) =>{
+    res.send("hi");
+})
+app.listen(port, '0.0.0.0', () => {
     console.log(`Serever running on port ${port}` );
 });
-
-
