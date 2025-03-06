3dc844d13dc7453e1e97b6f1305e631b0cacd01c
Aryan Kohli <aryankohli0805@gmail.com>
2025-03-06 11:39:22 +0530

    revived

--- mycontacts-frontend/vite-project/src/components/testuserdetails.jsx
+++ mycontacts-frontend/vite-project/src/components/testuserdetails.jsx
@@ -4,6 +4,7 @@ function TestUserDetails() {
   return (
    <div className="instructionsBoxStyle">
               <p>Test User email: testuser@email.com <br/> Test User password: testpassword  </p>
+              <p style={{ color: "red", fontFamily: "'Courier New', Courier, monospace",fontSize:"11px", textAlign: "left" }}>Not Working? Possible Reasons: <ol><li>The Backend server (deployed on Render) might have slept due to long inactivity. It should load after some time in this case.</li><li>The MongoDB database might have slept because of long inactivity.In this case or otherwise, feel free to write to me at aryankohli0805@gmail.com. I Love reading emails :)</li></ol></p>
    </div>
   );
 }
